6335930f90291ab91cd15e33832398fc590277b8
LogCreative <logcreative@outlook.com>
2023-11-10 14:34:03 +0800

    add bad code

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -26,6 +26,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Main calculator = new Main();
+
+		You are handsome.
 		
 		// Test cases
 		System.out.println("Test Case 1 (5 + 5): " + calculator.add(5, 5));
